c5e8128e262d6b77eb36b6050d28f6ec18e59940
Karthikraj <64545979+Karthikraj96@users.noreply.github.com>
2020-08-14 18:58:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var btn = document.querySelector(".sai");
+var getText = document.querySelector(".getText");
+var content = document.querySelector(".getcontent");
+var editorContent = document.querySelector(".editor");
+
+btn.addEventListener("click", function() {
+  var s = editorContent.innerHTML;
+  content.style.display = "block";
+  content.textContent = s;
+});
+
+getText.addEventListener("click", function() {
+  const old = editorContent.textContent;
+  content.style.display = "block";
+  content.textContent = old;
+});
+
+
+
